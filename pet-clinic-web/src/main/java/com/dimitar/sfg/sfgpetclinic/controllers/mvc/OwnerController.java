5fcd4434fea3f1b3c0ab9b247f3e42714214adad
divukman <divukman@gmail.com>
2019-07-19 13:20:48 +0200

    Owners list and rest api

--- pet-clinic-web/src/main/java/com/dimitar/sfg/sfgpetclinic/controllers/mvc/OwnerController.java
+++ pet-clinic-web/src/main/java/com/dimitar/sfg/sfgpetclinic/controllers/mvc/OwnerController.java
@@ -1,6 +1,9 @@
 package com.dimitar.sfg.sfgpetclinic.controllers.mvc;
 
+import com.dimitar.sfg.sfgpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
